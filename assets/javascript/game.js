b0c0578722ed055aacb800a6ac199d67aa47e1a5
bander <45398360+BenMakesCodeWork@users.noreply.github.com>
2019-02-09 22:46:08 -0600

    background image and word guess functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,141 @@
-var car = {
+var game = {
+    guessWordIndex: 0,
+    gameIndex: 0,
+    gameStarted: false,
+    score: 0,
+    wordGuessed: false,
     guessWordList: ["Gazorpazorp", "Rick", "Morty"],
-    guessWord1: ["G","a","z","o","r","p","a","z","o","r","p"],
-    guessWord2: ["R","i","c","k"],
-    guessWord3: ["M","o","r","t","y"],
-    // dualArray: [
-    //     ["Gazorpazorp"],["g","a","z","o","r","p","a","z","o","r","p"],
-    //     ["Rick"],["R","i","c","k"],
-    //     ["Morty"],["M","o","r","t","y"]
-    // ],
-    guessWordIndex: 2,
+    guessWordArray: [],
+    alphabet: [
+        ["a",0],
+        ["b",0],
+        ["c",0],
+        ["d",0],
+        ["e",0],
+        ["f",0],
+        ["g",0],
+        ["h",0],
+        ["i",0],
+        ["j",0],
+        ["k",0],
+        ["l",0],
+        ["m",0],
+        ["n",0],
+        ["o",0],
+        ["p",0],
+        ["q",0],
+        ["r",0],
+        ["s",0],
+        ["t",0],
+        ["u",0],
+        ["v",0],
+        ["w",0],
+        ["x",0],
+        ["y",0],
+        ["z",0],
+    ],
 
-    createCharList: function(x, y) {
+    createCharList: function(x) {
 
-        var ul = document.getElementById("guessWordCharList");
-        var li = document.createElement("li");
+        var element = document.createElement("div");
+        element.className = "blank";
+        element.id = x;
+        element.appendChild(document.createTextNode('-'));
+        document.getElementById('guessWordBox').appendChild(element);
 
-        li.appendChild(document.createTextNode(x));
-        ul.appendChild(li);
-        li.setAttribute("id", y)
+    },
+
+    populateGuessWordArray: function(newSize, guessWord) {
+
+        this.guessWordArray = [];
+
+        for (i = 0; i < newSize; i++) {
+            this.guessWordArray.push(guessWord.charAt(i));
+        }
     },
 
     // function to choose the next word in the list of guess words and create list items for each character
     populateBlanks: function() {
 
-        guessWord = this.guessWordList[this.guessWordIndex];
+        //clear list
+        var p = document.getElementById("guessWordBox");
+        while(p.firstChild) p.removeChild(p.firstChild);
 
-        alert("Guessword: " + guessWord);
+        val = this.guessWordList[this.guessWordIndex]
+        size = val.length;
 
-        for (i = 0; i < this.guessWord2.length; i++) {
-            this.createCharList("__ ", i);
+        this.populateGuessWordArray(size,val);
+
+        for (i = 0; i < size; i++) {
+            this.createCharList(i);
         }
+
+        this.guessWordIndex++;
     },
 
     checkCharacter: function(x) {
 
-        var textnode = document.createTextNode("Water");
-
-        alert("Keypress: " + x);
-        for (i = 0; i < this.guessWord2.length; i++) {
-            //alert("i - " + i);
-            //alert("keypress - " + x);
-            if(this.guessWord2[i] == x) {
-                var item = document.getElementById("myList").childNodes[0];
-                item.replaceChild(textnode, item.childNodes[0]);
-                //document.getElementById("2").node.nodeValue = "rar";
-                //document.getElementById(i).childNodes[0].replaceChild(textnode, this.guessWord2[i]);
-                //document.getElementById(i).appendChild(document.createTextNode("rar"));
+        for (i = 0; i < this.guessWordArray.length; i++) {
+            if(this.guessWordArray[i].toLowerCase() == x.toLowerCase()) {
+                document.getElementById(i).childNodes[0].nodeValue = this.guessWordArray[i]
             }
         }
+
+        this.logCharacterUsed(x);
+        this.checkWord();
+
+    },
+
+    logCharacterUsed: function(x) {
+        
+        for(i = 0; i < this.alphabet.length; i++) {
+            if(x.toLowerCase() == this.alphabet[i][0]) {
+                this.alphabet[i][1] = 1;
+            }
+        }
+        
+        console.log(this.alphabet);
+    },
+
+    checkWord: function() {
+
+        var p = document.getElementById("guessWordBox");
+        var items = p.getElementsByTagName("div");
+        this.wordGuessed = true;
+
+        for (i = 0; i < items.length; i++) {
+            if (items[i].childNodes[0].nodeValue == "-") {
+                this.wordGuessed = false;
+            }
+        }   
+
+    },
+
+    finished: function() {
+        alert("You have finished the game");
     }
 
 };
 
+
 document.onkeyup = function(event) {
 
-      var keyPress = event.key;
-
-      console.log(keyPress)
-
-        if (keyPress == "c") {
-            car.createCharList();
-        } else if (keyPress == "p") {
-            car.populateBlanks();
+    if (!game.gameStarted) {
+        alert("Guess the Word using the hint.");
+        game.populateBlanks();   
+        game.gameStarted = true; 
+    } else if (game.wordGuessed === true) {
+        if (game.guessWordIndex == game.guessWordList.length) {
+            game.finished();
+        } else {
+            game.populateBlanks();
+            game.wordGuessed = false;
         }
-
-        car.checkCharacter(keyPress);
+    } else {
+        var keyPress = event.key;
+        game.checkCharacter(keyPress);
+        if (game.wordGuessed) {
+            alert("You guessed the word.  Press any key to continue");
+        }
     }
-
-
+}
